c3af635f9f7c014e25a8a904784269f0ced5fd3b
SoheilMV <47953001+SoheilMV@users.noreply.github.com>
2021-01-18 14:20:09 +0330

    Add files via upload

---
+++ CPM/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace CPM
+{
+    internal class Data
+    {
+        private string _key = string.Empty;
+
+        /// <summary>The unix timestamp of the check completion.</summary>
+        public int UnixDate { get; set; }
+
+        /// <summary>The timestamp of the check completion as a formatted date.</summary>
+        public string Timestamp { get { return (new DateTime(1970, 1, 1)).AddSeconds(UnixDate).ToShortDateString(); } }
+
+        /// <summary>The timestamp of the check completion as a DateTime object.</summary>
+        public DateTime Time { get { return (new DateTime(1970, 1, 1)).AddSeconds(UnixDate); } }
+
+        public Data()
+        {
+            UnixDate = (int)Math.Round((DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds);
+            _key = KeyGenerator(10);
+        }
+
+        private string KeyGenerator(int length, string valid = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890")
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            StringBuilder res = new StringBuilder();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ CPM/DataPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CPM
+{
+    public class DataPool
+    {
+        private readonly object _obj = new object();
+        private List<Data> _datas = new List<Data>();
+
+        /// <summary>The checks per minute.</summary>
+        public int CPM
+        {
+            get
+            {
+                lock (_obj)
+                {
+                    var temp = 0;
+                    if (_datas.Count == 0)
+                    {
+                        return temp;
+                    }
+                    try
+                    {
+                        for (int i = _datas.Count() - 1; i >= 0; i--)
+                        {
+                            if ((DateTime.Now - _datas[i].Time).TotalSeconds > 60)
+                            {
+                                break;
+                            }
+                            temp++;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                    return temp;
+                }
+            }
+        }
+
+        public void Add()
+        {
+            _datas.Add(new Data());
+        }
+
+        public void Clear()
+        {
+            _datas.Clear();
+        }
+    }
+}
