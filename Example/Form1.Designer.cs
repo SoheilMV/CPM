e06dd341458d51ae801d8996233ad80fcf922b9e
SoheilMV <47953001+SoheilMV@users.noreply.github.com>
2021-01-18 14:24:10 +0330

    Add files via upload

---
+++ Example/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblCpm = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 12);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(168, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblCpm
+            // 
+            this.lblCpm.AutoSize = true;
+            this.lblCpm.Location = new System.Drawing.Point(12, 47);
+            this.lblCpm.Name = "lblCpm";
+            this.lblCpm.Size = new System.Drawing.Size(45, 13);
+            this.lblCpm.TabIndex = 1;
+            this.lblCpm.Text = "CPM : 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Location = new System.Drawing.Point(12, 65);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(65, 13);
+            this.lblCheck.TabIndex = 1;
+            this.lblCheck.Text = "Checked : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 94);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.lblCpm);
+            this.Controls.Add(this.btnTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Checks Per Minute";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblCpm;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCheck;
+    }
+}
+
---
+++ Example/Form1.cs
@@ -0,0 +1,58 @@
+﻿using CPM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        DataPool dp = new DataPool();
+        int checks = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            dp.Clear();
+            checks = 0;
+            for (int i = 0; i < 50; i++)
+            {
+                Thread thread = new Thread(() => { Config(); }) { IsBackground = true };
+                thread.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblCheck.Text = $"Checked : {checks}";
+            lblCpm.Text = $"CPM : {dp.CPM}";
+        }
+
+        private void Config()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                checks++;
+                dp.Add();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
